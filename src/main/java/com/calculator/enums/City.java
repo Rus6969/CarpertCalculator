14cd2aca52f57c540659f4282150513ffd5b2372
Russell_Samatov <russam4515@gmail.com>
2020-10-30 18:37:05 -0400

    enums  added

--- src/main/java/com/calculator/enums/City.java
+++ src/main/java/com/calculator/enums/City.java
@@ -1,7 +1,14 @@
 package com.calculator.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum City {
 
-
+    DALLAS("Dallas"),AUSTIN("Austin"),SAN_ANTONIO("San Antonio"),ARLINGTON("Arlington"),
+    FAIRFAX("Fairfax"),MCLEAN("McLean");
+    private final String value;
 
 }
